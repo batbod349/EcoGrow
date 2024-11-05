ef9c18825f1f1d8cde05b5612319a70b3d3dd334
Alice GUEGAN <91895630+gueganalice@users.noreply.github.com>
2024-11-05 09:34:15 +0100

    Starter changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./assets/**/*.js",
-    "./templates/**/*.html.twig",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./assets/**/*.js",
+        "./templates/**/*.html.twig",
+    ],
+    theme: {
+        colors: {
+            'black': 'var(--black)',
+            'white': 'var(--white)',
+            'greenDark': 'var(--greenDark)',
+            'greenLight': 'var(--greenLight)',
+            'greenMint': 'var(--greenMint)',
+        },
+        extend: {},
+    },
+    plugins: [],
 }
