3fd5b8ca9afa62d317d03d8be76f6d76aec0ff88
nathael <nathael.stalder@gmail.com>
2024-11-07 15:57:41 +0100

    friends

--- src/Repository/FriendsRepository.php
+++ src/Repository/FriendsRepository.php
@@ -29,6 +29,43 @@ class FriendsRepository extends ServiceEntityRepository
             ->getResult();
     }
 
+    public function isFriendshipExists($user1Id, $user2Id): bool
+    {
+        $qb = $this->createQueryBuilder('f');
+        $qb->where('f.user1 = :user1 AND f.user2 = :user2')
+            ->orWhere('f.user1 = :user2 AND f.user2 = :user1')
+            ->andWhere('f.accepted = true')
+            ->setParameter('user1', $user1Id)
+            ->setParameter('user2', $user2Id);
+
+        return (bool) $qb->getQuery()->getOneOrNullResult();
+    }
+
+    public function isFriendshipRequest(int $user1Id, int $user2Id): ?Friends
+    {
+        $qb = $this->createQueryBuilder('f')
+            ->where('f.user1 = :user1 AND f.user2 = :user2')
+            ->orWhere('f.user1 = :user2 AND f.user2 = :user1')
+            ->setParameter('user1', $user1Id)
+            ->setParameter('user2', $user2Id)
+            ->getQuery();
+
+        return $qb->getOneOrNullResult();
+    }
+
+    public function acceptFriendship(int $user1Id, int $user2Id): void
+    {
+        $qb = $this->createQueryBuilder('f')
+            ->update()
+            ->set('f.accepted', ':accepted')
+            ->where('f.user1 = :user2 AND f.user2 = :user1')
+            ->setParameter('accepted', true)
+            ->setParameter('user1', $user1Id)
+            ->setParameter('user2', $user2Id)
+            ->getQuery();
+        $qb->execute();
+    }
+
 //    /**
 //     * @return Friends[] Returns an array of Friends objects
 //     */
