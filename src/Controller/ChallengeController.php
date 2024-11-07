81f5afc8f63df354d1d58474786048614a48df73
nathael <nathael.stalder@gmail.com>
2024-11-07 21:09:16 +0100

    hjkfvdgfjkmqdegsfhjkehqlrifojdsqbhfvglrem

--- src/Controller/ChallengeController.php
+++ src/Controller/ChallengeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Experiences;
+use App\Entity\Quest;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -20,5 +23,36 @@ class ChallengeController extends AbstractController
             'dailyQuests' => $dailyQuests,
             'monthlyQuests' => $monthlyQuests,
         ]);
+    }
+
+    #[Route('/challenge/validate/{id}', name: 'app_challenge_validate')]
+    public function validate(Quest $quest, EntityManagerInterface $entityManager): Response
+    {
+
+        $user = $this->getUser();
+        if (!$user) {
+            return $this->redirectToRoute('app_login');
+        }
+        if ($user->getQuests()->contains($quest)) {
+            // Return a response indicating the quest has already been validated
+            $this->addFlash('warning', 'You have already validated this quest.');
+            return $this->redirectToRoute('app_challenge');
+        }
+        $user->addQuest($quest);
+
+        $experience = new Experiences();
+        $experience->setQuantity($quest->getRewards());
+        $experience->setDate(new \DateTime());
+        $experience->setType(1); // Assuming type 1 for this case
+        $experience->setSource('Quest');
+        $experience->setUser($user);
+        $experience->setQuest($quest);
+        $experience->setCompletedAt(new \DateTime());
+
+        $entityManager->persist($experience);
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_challenge');
     }
 }
