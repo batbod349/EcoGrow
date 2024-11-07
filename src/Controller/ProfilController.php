e382b8e34f8e7d23e454e34ed433b0a1bae026c9
nathael <nathael.stalder@gmail.com>
2024-11-07 08:44:58 +0100

    ProfilController.php

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\BadgesRepository;
+use App\Repository\FriendsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +11,21 @@ use Symfony\Component\Routing\Attribute\Route;
 class ProfilController extends AbstractController
 {
     #[Route('/profil', name: 'app_profil')]
-    public function index(): Response
+    public function index(FriendsRepository $friendsRepository, BadgesRepository $badgesRepository): Response
     {
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('app_login');
+        }
+        $user = $this->getUser();
+        $userBadges = $user->getBadges();
+        $badges = $badgesRepository->findAll();
+        $friends = $friendsRepository->getFriends($user->getId());
+
         return $this->render('profil/index.html.twig', [
-            'controller_name' => 'ProfilController',
+            'user' => $user,
+            'userBadges' => $userBadges,
+            'badges' => $badges,
+            'friends' => $friends,
         ]);
     }
 }
