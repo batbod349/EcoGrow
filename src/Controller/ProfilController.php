f8397a43e1da39b2e35c690bc439d4b50aa793d2
nathael <nathael.stalder@gmail.com>
2024-11-07 14:26:06 +0100

    profile edit

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -1,9 +1,12 @@
 <?php
 namespace App\Controller;
 
+use App\Form\ProfilEditType;
 use App\Repository\BadgesRepository;
 use App\Repository\FriendsRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -25,6 +28,28 @@ class ProfilController extends AbstractController
             'userBadges' => $userBadges,
             'badges' => $badges,
             'friends' => $friends,
+        ]);
+    }
+
+    #[Route('/profil/edit', name: 'app_profil_edit')]
+    public function edit(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        $user = $this->getUser();
+        $form = $this->createForm(ProfilEditType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($user);
+            $entityManager->flush();
+            return $this->redirectToRoute('app_profil');
+        }
+
+        return $this->render('profil/edit.html.twig', [
+            'ProfilEdit' => $form->createView(),
         ]);
     }
 }
