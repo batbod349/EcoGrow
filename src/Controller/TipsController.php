3ea0d6a5cf852cc2c6a038dfaf118a6c2944e94f
SherzodK1 <sherzodozodkhashimov@gmail.com>
2024-11-08 01:35:45 +0100

    la page tips affiche des truc sa grand mére 😎😎

--- src/Controller/TipsController.php
+++ src/Controller/TipsController.php
@@ -12,11 +12,14 @@ class TipsController extends AbstractController
     #[Route('/tips', name: 'app_tips')]
     public function index(TipsRepository $tipsRepository): Response
     {
-        $popularTips = $tipsRepository->findPopularTips();
-        $courseTips = $tipsRepository->findCourseTips();
+        // Récupération des tips selon leur type
+        $consommationTips = $tipsRepository->findBy(['type' => 'Consommation et Achats Responsables']);
+        $energieTips = $tipsRepository->findBy(['type' => 'Économies d’Énergie et Transport Écoresponsable']);
+        
+        // Passer les données au template
         return $this->render('tips/index.html.twig', [
-            'popularTips' => $popularTips,
-            'courseTips' => $courseTips,
+            'consommationTips' => $consommationTips,
+            'energieTips' => $energieTips,
         ]);
     }
 }
